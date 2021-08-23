fde2b772cced84bd3ae6353b66e2574f1b48ae73
Luke Michalakis <michalakisluke@gmail.com>
2021-08-23 13:54:37 -0400

    <Improve form submission, seperate functions>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,32 @@
 var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function (event) {
+var taskFormHandler = function (event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    console.dir(taskNameInput);
-    console.log(taskTypeInput);
 
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    createTaskE1(taskDataObj);
+};
+
+var createTaskE1 = function(taskDataObj) {
     // create list item
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
 
     //create div to hold task info and add to list item
     var taskInfoE1 = document.createElement("div");
-
-    //give it a class name
     taskInfoE1.className = "task-info";
-
-    //add HTML content to div
-    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemE1.appendChild(taskInfoE1);
 
-    // add entire list item to list
+    // add entire item to list
     tasksToDoE1.appendChild(listItemE1);
 };
 
-formE1.addEventListener("submit", createTaskHandler);
+formE1.addEventListener("submit", taskFormHandler);
